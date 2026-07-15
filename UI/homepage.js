fad4db6d4c8b83d07ad98ea61773f196deb85a0b
Fletcher Thompson <fept04@gmail.com>
2026-07-15 17:26:37 +0100

    UI knows source, target and piece moved from drag-and-drop. Now need promotion piece choice

--- UI/homepage.js
+++ UI/homepage.js
@@ -1,7 +1,13 @@
-import { extractNotFEN } from "./utilities.js";
+import { ExtractNotFENFromBoard, GetPieceIndex, GetSquareIndex, PieceAtIndex } from "./utilities.js";
 
 // Not using FEN notation - this is in a minimal form such that the board can be displayed on the screen
 export let notFEN = "rnbqkbnrpppppppp--------------------------------PPPPPPPPRNBQKBNR";
+
+let source = 0;
+let target = 0;
+// 0 - not a promo ||| 1 - knight ||| 2 - bishop ||| 3 - rook ||| 4 - queen ||| 5 - king
+let pawnPromoType = 0;
+let squareContainingDraggingMouse = 0;
 
 function Homepage(){
 
@@ -52,12 +58,42 @@ function Homepage(){
     boardSquareHTML += `<div class="piece-container piece-container-${i} contains-piece-${pieceId}">`;
     if(pieceFileName){
       boardSquareHTML += `
-        <img src="./Assets/${pieceFileName}.svg" height="65" width="65">
+        <img src="./Assets/${pieceFileName}.svg" height="65" width="65" class="piece-svg piece-svg-${i}">
       `;
     }
     boardSquareHTML += `</div>`;
     boardSquare.innerHTML = boardSquareHTML;
   }
+
+  // Squares switch squareContainingDraggingMouse when drag mouse is dragged into them
+  const squares = document.querySelectorAll(".board-square");
+  squares.forEach((square) => {
+    square.addEventListener("dragenter", () => {
+      squareContainingDraggingMouse = GetSquareIndex(square);
+    });
+  });
+
+  // Make the pieces drag-and-droppable
+  const pieces = document.querySelectorAll(".piece-svg");
+  pieces.forEach((piece) => {
+    piece.addEventListener("dragstart", () => {
+      piece.classList.add("see-through");
+      source = GetPieceIndex(piece);
+    });
+
+    // On drag end is the move sent to the engine
+    piece.addEventListener("dragend", () => {
+      piece.classList.remove("see-through");
+      target = squareContainingDraggingMouse;
+
+      // Handle the extra data for pawn promotion moves
+      if(0 <= target && target <= 7 && PieceAtIndex(source) === 1){ HandlePromoMove(); }
+      else if(56 <= target && target <= 63 && PieceAtIndex(source) === 7){ HandlePromoMove(); }
+
+      console.log(source, "to", target);
+      console.log("piece:", PieceAtIndex(source));
+    });
+  })
 }
 
 Homepage();
--- UI/utilities.js
+++ UI/utilities.js
@@ -1,5 +1,32 @@
 import { notFEN } from "./homepage.js";
 
-export function extractNotFEN(){
+export function ExtractNotFENFromBoard(){
   
+}
+
+export function GetSquareIndex(square){
+  for(let i = 0; i < 64; i++){
+    if(square.classList.contains(`board-square-${i}`)){
+      return i;
+    }
+  }
+}
+
+export function GetPieceIndex(piece){
+  for(let i = 0; i < 64; i++){
+    if(piece.classList.contains(`piece-svg-${i}`)){
+      return i;
+    }
+  }
+}
+
+export function HandlePromoMove(){
+  // handle it
+}
+
+export function PieceAtIndex(index){
+  const squareWithIndex = document.querySelector(`.piece-container-${index}`);
+  for(let piece = 0; piece <= 12; piece++){
+    if(squareWithIndex.classList.contains(`contains-piece-${piece}`)){ return piece; }
+  }
 }
